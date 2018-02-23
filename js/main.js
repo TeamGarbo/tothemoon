776c47ec4e6328fbae7e01473cbae050196bf729
mallocc <gh@alexeverett.uk>
2018-02-23 23:29:40 +0000

    fixed

--- js/main.js
+++ js/main.js
@@ -6,9 +6,9 @@ document.body.appendChild(app.view);
 app.stage.interactive = true;
 
 // create some textures from an image path
-var textureButton = PIXI.Texture.fromImage('https://dl.dropboxusercontent.com/s/mi2cibdajml8qj9/arrow_wait.png?dl=0');
-var textureButtonDown = PIXI.Texture.fromImage('https://dl.dropboxusercontent.com/s/m0x11c91wazehyp/arrow_error.png?dl=0');
-var textureButtonOver = PIXI.Texture.fromImage('https://dl.dropboxusercontent.com/s/1kuhddt8p9tr0k8/arrow_wait.png?dl=0');
+var textureButton = PIXI.Texture.fromImage('res\buttonup.png');
+var textureButtonDown = PIXI.Texture.fromImage('res\buttondown.png');
+var textureButtonOver = PIXI.Texture.fromImage('res\buttonup.png');
 
 var button = new PIXI.Sprite(textureButton);
 button.buttonMode = true;
@@ -22,31 +22,22 @@ button.interactive = true;
 button.buttonMode = true;
 
 button
-// Mouse & touch events are normalized into
-// the pointer* events for handling different
-// button events.
 .on('pointerdown', onButtonDown)
 .on('pointerup', onButtonUp)
 .on('pointerupoutside', onButtonUp)
 .on('pointerover', onButtonOver)
 .on('pointerout', onButtonOut);
 
-// Use mouse-only events
-// .on('mousedown', onButtonDown)
-// .on('mouseup', onButtonUp)
-// .on('mouseupoutside', onButtonUp)
-// .on('mouseover', onButtonOver)
-// .on('mouseout', onButtonOut)
-
-// Use touch-only events
-// .on('touchstart', onButtonDown)
-// .on('touchend', onButtonUp)
-// .on('touchendoutside', onButtonUp)
-
 // add it to the stage
 app.stage.addChild(button);
 
 var incr = 0;
+
+function onButtonDown() {
+	this.isdown = true;
+	this.texture = textureButtonDown;
+	this.alpha = 1;
+}
 
 function onButtonUp() {
 
@@ -114,4 +105,3 @@ app.ticker.add(function () {
 
 	thing.rotation = count * 0.1;
 });
-
